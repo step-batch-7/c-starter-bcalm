082b0a01ede4ce6c81b2d43e60e028a6eba8b1a6
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-03-28 21:04:29 +0530

    Added multiplication function

--- src/loops.c
+++ src/loops.c
@@ -2,8 +2,9 @@
 
 unsigned long int factorial(int);
 int fibonacci(int);
-int display_odd_series(int);
-int display_alternative_number(int, int);
+int print_odds(int);
+int print_alternative_number(int, int);
+int multiplication_table(int, int);
 
 unsigned long int factorial(int number)
 {
@@ -29,7 +30,7 @@ int fibonacci(int fibonacciTerm)
   return 0;
 }
 
-int display_alternative_number(int start_from, int upto)
+int print_alternative_number(int start_from, int upto)
 { 
   while(start_from < upto)
   {
@@ -39,29 +40,44 @@ int display_alternative_number(int start_from, int upto)
   return 0;
 }
 
-int display_odd_series(int number)
+int print_odds(int number)
 {
-  return display_alternative_number(1, number);
+  return print_alternative_number(1, number);
 }
 
-int display_even_series(int number)
+int print_evens(int number)
 {
-  return display_alternative_number(2, number);
+  return print_alternative_number(2, number);
+}
+
+int multiplication_table(int mth_term, int nth_term)
+{
+  for(int num =1; num <= nth_term; num++)
+  {
+    printf("%d * %d = %d\n", mth_term, num, mth_term*num);
+  }
+  return 0;
 }
 
 int main(void)
 {
-  int number;
+  int number, number1, number2;
   printf("Enter a number for calculating factorial: ");
   scanf("%d", &number);
   printf("Factorial of %d is %lu \n", number, factorial(number));
-  printf("Enter a number for series: ");
+  printf("Enter a number for printing series: ");
   scanf("%d", &number);
   printf("Fibonacci Series: \n");
   fibonacci(number);
   printf("Odd series: \n");
-  display_odd_series(number);
+  print_odds(number);
   printf("Even series: \n");
-  display_even_series(number);
+  print_evens(number);
+  printf("Enter two numbers for multiplication table:\n");
+  printf("Enter first number: ");
+  scanf("%d", &number1);
+  printf("Enter second number: ");
+  scanf("%d", &number2);
+  multiplication_table(number1, number2);
   return 0;
 }

c64b1390aa015f03b29e21c35c9e45478e84ddd7
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-03-28 15:46:04 +0530

    Added isOdd function

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 unsigned char isEven(int);
+unsigned char isOdd(int);
 
 unsigned char isEven(int number){
   return number % 2 == 0;
+}
+
+unsigned char isOdd(int number){
+  return (!isEven(number));
 }
 
 int main(void){

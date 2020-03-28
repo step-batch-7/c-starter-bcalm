45733c63d6f64b25921d03e635cad1b9f75bc68f
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-03-28 15:07:54 +0530

    Added: script for compiling and running functions

---
+++ run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+rm -rf bin
+mkdir -p bin
+for i in `ls src/*.c`; do
+  executable=`echo ${i} | sed 's/src\///' | sed "s/.c$//"`
+  gcc -o bin/${executable} ${i} 2>/dev/null && ./bin/${executable}
+done
+

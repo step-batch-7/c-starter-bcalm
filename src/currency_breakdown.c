a9163ab21d236a845ee586560bfa01f68ba39302
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-03-29 19:29:32 +0530

    Refactor currency_breakdown function

--- src/currency_breakdown.c
+++ src/currency_breakdown.c
@@ -11,27 +11,37 @@ long int print_currency(long int money, int note)
   return money % note;
 }
 
-int currency_breakdown(long int rupee)
+int currency_breakdown(long int amount)
 {
-  long int remaining_money = rupee;
-  remaining_money = print_currency(remaining_money, 2000);
-  remaining_money = print_currency(remaining_money, 500);
-  remaining_money = print_currency(remaining_money, 200);
-  remaining_money = print_currency(remaining_money, 100);
-  remaining_money = print_currency(remaining_money, 50);
-  remaining_money = print_currency(remaining_money, 20);
-  remaining_money = print_currency(remaining_money, 10);
-  remaining_money = print_currency(remaining_money, 5);
-  remaining_money = print_currency(remaining_money, 2);
-  remaining_money = print_currency(remaining_money, 1);
+  long int remaining_money = amount;
+  int note = 2000;
+  remaining_money = print_currency(remaining_money, note);
+  note = 500;
+  remaining_money = print_currency(remaining_money, note);
+  note = 200;
+  remaining_money = print_currency(remaining_money, note);
+  note = 100;
+  remaining_money = print_currency(remaining_money, note);
+  note = 50;
+  remaining_money = print_currency(remaining_money, note);
+  note = 20;
+  remaining_money = print_currency(remaining_money, note);
+  note = 10;
+  remaining_money = print_currency(remaining_money, note);
+  note = 5;
+  remaining_money = print_currency(remaining_money, note);
+  note = 2;
+  remaining_money = print_currency(remaining_money, note);
+  note = 1;
+  remaining_money = print_currency(remaining_money, note);
   return 0;
 }
 
 int main(void)
 {
-  long int rupee;
-  printf("Enter Rupees which you want to withdraw: ");
-  scanf("%ld", &rupee);
-  currency_breakdown(rupee);
+  long int amount;
+  printf("Enter Amount ");
+  scanf("%ld", &amount);
+  currency_breakdown(amount);
   return 0;
 }

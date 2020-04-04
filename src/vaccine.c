e6b665c15c2b1799c1a038161c84d9cd8679b509
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-04-04 12:49:38 +0530

    Added decryption_message function

---
+++ src/vaccine.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void decryption(int *encrypted_message, char *decrypted_message, int message_length)
+{
+  for (int rotation = 1; rotation <= 26; rotation++)
+  {
+    for (int term = 0; term < 9; term++)
+    {
+      decrypted_message[term] = encrypted_message[term] > 109 ? encrypted_message[term] - rotation : encrypted_message[term] + rotation;
+    }
+  }
+}
+int main(void)
+{
+  char decrypted_message[9];
+  int encrypted_message[9] = {116, 98, 32, 112, 98, 101, 98, 97, 110};
+  int message_length = 9;
+  decryption(encrypted_message, decrypted_message, message_length);
+  return 0;
+}
+
+//Correct rotation number is 13
+//Answer is go corona
